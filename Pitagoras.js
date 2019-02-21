f915443a100e0d7de263bb698cb2427e31ef050c
Jserratos <47637148+Jserratos@users.noreply.github.com>
2019-02-21 09:09:30 -0600

    Create Pitagoras.js

---
+++ Pitagoras.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+	var LadoA= 5
+	var LadoB= 5;
+	C = sqrt(LadoA*LadoA + LadoB*LadoB);
+	console.log(C);
+}
+
+function Hipotenusa(a,b){
+	var LadoC = C
+			return C;
+}
